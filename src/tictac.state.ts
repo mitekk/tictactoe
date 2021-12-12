7229f84db4857c291be8ce82666387133651075f
Inbal <mitekk@gmail.com>
2021-12-12 18:55:23 +0200

    improve state read

--- src/tictac.state.ts
+++ src/tictac.state.ts
@@ -1,114 +1,62 @@
 import _ from "lodash";
 import { Payload } from "./types/payload.type";
-import { ReadResult } from "./types/readResult.type";
-import { DIRECTIONPAIRS, GRID_LENGTH } from "./config/constants";
+import { GRID_LENGTH } from "./config/constants";
 import { StateReadResult } from "./types/stateReadResult.type";
 
 export default {
   read: ({ index, fields, playerTag = "o" }: Payload) => {
-    // try set player field to read result
-    const setPlayer = (pivot: number, result: StateReadResult) => {
-      if (fields[pivot]?.player === playerTag) {
-        result.indexes.push(pivot);
-        result.count++;
-      }
-      return result;
-    };
+    type path = "criss" | "cross" | "horizontal" | "vertical";
+    const directions: path[] = ["criss", "cross", "horizontal", "vertical"];
 
-    // count number of playerTags in a single direction
-    const findPaths = (
-      pivot: number,
-      direction: number,
-      indexScope: number[]
-    ): StateReadResult => {
-      if (
-        pivot >= fields.length ||
-        pivot < 0 ||
-        !_.includes(indexScope, pivot)
-      ) {
-        return {
+    const rowChunks = _.chunk(fields, GRID_LENGTH);
+
+    const getPathsBy = (index: number) => ({
+      criss: index % 4 === 0 ? [0, 4, 8] : [-1],
+      cross: index % 2 === 0 ? [2, 4, 6] : [-1],
+      horizontal: rowChunks
+        .filter((chunk) => chunk.findIndex((i) => i.index === index) !== -1)
+        .flatMap((chunk) => chunk.map((field) => field.index)),
+      vertical: rowChunks
+        .map((_, colIndex) => rowChunks.flatMap((row) => row[colIndex].index))
+        .filter((chunk) => chunk.findIndex((i) => i === index) !== -1)
+        .flat(),
+    });
+
+    const scopedIndexes = _.uniq(
+      directions.map((direction) => getPathsBy(index)[direction])
+    );
+
+    const scopedResults = scopedIndexes.map((indexesToInspect) =>
+      indexesToInspect.reduce(
+        (result: StateReadResult, index) => {
+          if (fields[index]?.player === playerTag) {
+            result.indexes.push(index);
+            result.count++;
+          }
+          return result;
+        },
+        {
           indexes: [],
           count: 0,
-        };
-      }
-
-      const result = findPaths(pivot + direction, direction, indexScope);
-
-      return setPlayer(pivot, result);
-    };
-
-    // count number of playerTags in all directions
-    return Object.entries(DIRECTIONPAIRS)
-      .map(([key, directions]) => {
-        let indexScope;
-
-        if (key === "criss") {
-          // read diagonals only on even numbers
-          if (index % 4 === 0) {
-            const crissIndexes = [0, 4, 8];
-
-            // TODO: make sure if needed
-            indexScope = _.includes(crissIndexes, index) && crissIndexes;
-          }
-        } else if (key === "cross") {
-          // read diagonals only on even numbers
-          if (index % 2 === 0) {
-            const crossIndexes = [2, 4, 6];
-
-            // TODO: make sure if needed
-            indexScope = _.includes(crossIndexes, index) && crossIndexes;
-          }
-        } else if (key === "horizontal") {
-          // get row of scoped indexes
-          indexScope = _.chunk(fields, GRID_LENGTH)
-            .filter((chunk) => chunk.findIndex((i) => i.index === index) !== -1)
-            .flatMap((chunk) => chunk.map((field) => field.index));
-        } else if (key === "vertical") {
-          // allow all - direction volume covers it
-          indexScope = _.range(Math.pow(GRID_LENGTH, 2));
         }
+      )
+    );
 
-        if (indexScope) {
-          const [directionOne, directionTwo] = directions;
-          const directionOneResult = findPaths(
-            index + directionOne,
-            directionOne,
-            indexScope
-          );
-          const directionTwoResult = findPaths(
-            index + directionTwo,
-            directionTwo,
-            indexScope
-          );
-
-          const result = {
-            indexes: [
-              ...directionOneResult.indexes,
-              ...directionTwoResult.indexes,
-            ],
-            count: directionOneResult.count + directionTwoResult.count,
-          };
-
-          return {
-            [key]: setPlayer(index, result),
-          };
-        }
-      })
-      .filter((d) => d);
+    return scopedResults;
   },
 
   // find a row with 3 playerTag fields
-  findWinner: (results: ReadResult) => {
+  findWinner: (results: StateReadResult[]) => {
     let winnerIndexes;
 
     const winnerDirections = results.filter((result) => {
-      const { count } = result![Object.keys(result!)[0]];
+      const { count } = result;
       return count === GRID_LENGTH;
     });
 
     if (winnerDirections.length > 0) {
       winnerIndexes = winnerDirections.flatMap((candidate) => {
-        const { indexes } = candidate![Object.keys(candidate!)[0]];
+        const { indexes } = candidate;
         return indexes;
       });
     }
--- src/types/readResult.type.ts
+++
@@ -1,3 +0,0 @@
-import { StateReadResult } from "./stateReadResult.type";
-
-export type ReadResult = ({ [x: string]: StateReadResult } | undefined)[];
